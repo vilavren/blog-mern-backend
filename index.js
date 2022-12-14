4ff58d3d9e5ceac42045fca3f8c8730f28749f8f
vitalii <vilavren@gmail.com>
2022-12-14 16:41:52 +0500

    added models User

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:wwwwww@cluster0.opvl2gi.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err))
 
 const app = express()
 
