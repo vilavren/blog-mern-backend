29a458abcc7644be3165ee19472726ba06c8901a
vitalii <vilavren@gmail.com>
2022-12-14 18:20:31 +0500

    added validation form

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
+import { validationResult } from 'express-validator'
+
+import { registerValidation } from './validations/auth.js'
 
 mongoose
   .connect(
@@ -13,24 +16,14 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
-
-app.post('/auth/login', (req, res) => {
-  console.log(req.body)
-
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullname: 'Иван Иванов',
-    },
-    'secret123'
-  )
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array())
+  }
 
   res.json({
     success: true,
-    token,
   })
 })
 
