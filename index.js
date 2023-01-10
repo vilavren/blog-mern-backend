5ef6bc64ebb51585ae89f277f4a72657ef42ca30
vitalii <vilavren@gmail.com>
2023-01-10 13:39:11 +0500

    Registration is completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
+import bcrypt, { hash } from 'bcrypt'
 import mongoose from 'mongoose'
 import { validationResult } from 'express-validator'
 
@@ -27,18 +27,33 @@ app.post('/auth/register', registerValidation, async (req, res) => {
 
     const password = req.body.password
     const salt = await bcrypt.genSalt(10)
-    const passwordHash = await bcrypt.hash(password, salt)
+    const hash = await bcrypt.hash(password, salt)
 
     const doc = new UserModel({
       email: req.body.email,
       fullName: req.body.fullName,
       avatarUrl: req.body.avatarUrl,
-      passwordHash,
+      passwordHash: hash,
     })
 
     const user = await doc.save()
 
-    res.json(user)
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      }
+    )
+
+    const { passwordHash, ...userData } = user._doc
+
+    res.json({
+      ...userData,
+      token,
+    })
   } catch (error) {
     console.log(error)
     res.status(500).json({
