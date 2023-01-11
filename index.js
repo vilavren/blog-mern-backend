f8962d8795719db5d85e4fd5ccd9c830255bf9c9
vitalii <vilavren@gmail.com>
2023-01-11 19:31:52 +0500

    Added authorization and refactor of code

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
-import bcrypt, { hash } from 'bcrypt'
 import mongoose from 'mongoose'
-import { validationResult } from 'express-validator'
-
 import { registerValidation } from './validations/auth.js'
-import UserModel from './models/User.js'
+import checkAuth from './utils/checkAuth.js'
+
+import * as UserControler from './controllers/UserControler.js'
 
 mongoose
   .connect(
@@ -15,57 +13,15 @@ mongoose
   .catch((err) => console.log('DB error', err))
 
 const app = express()
-
 app.use(express.json())
 
-app.post('/auth/register', registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array())
-    }
-
-    const password = req.body.password
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password, salt)
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    })
-
-    const user = await doc.save()
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      'secret123',
-      {
-        expiresIn: '30d',
-      }
-    )
-
-    const { passwordHash, ...userData } = user._doc
-
-    res.json({
-      ...userData,
-      token,
-    })
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({
-      message: 'Не удалось зарегистрироваться',
-    })
-  }
-})
+app.post('/auth/login', UserControler.login)
+app.post('/auth/register', registerValidation, UserControler.register)
+app.get('/auth/me', checkAuth, UserControler.getMe)
 
 app.listen(4444, (err) => {
   if (err) {
     return console.log(err)
   }
-
   console.log('Server OK')
 })
