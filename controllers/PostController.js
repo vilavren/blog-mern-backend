f86cf20e3014b59410d032172301356e93012594
vitalii <vilavren@gmail.com>
2023-01-19 00:18:03 +0500

    added all posts

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,18 @@
 import PostModel from '../models/Post.js'
 
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate('user').exec()
+
+    res.json(posts)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({
+      message: 'Не удалось получить статьи',
+    })
+  }
+}
+
 export const create = async (req, res) => {
   try {
     const doc = new PostModel({
