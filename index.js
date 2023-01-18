e3395d03e10fc9f49028e1931f9ab3d8e7a24976
vitalii <vilavren@gmail.com>
2023-01-19 00:35:29 +0500

    added one post

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post('/auth/register', registerValidation, UserController.register)
 app.get('/auth/me', checkAuth, UserController.getMe)
 
 app.get('/posts', PostController.getAll)
-// app.get('/posts/:id', PostController.getOne)
+app.get('/posts/:id', PostController.getOne)
 app.post('/posts', checkAuth, postCreateValidation, PostController.create)
 // app.delete('/posts', PostController.remove)
 // app.patch('/posts', PostController.update)
