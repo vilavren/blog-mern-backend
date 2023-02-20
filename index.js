a03360b5fbfaa735fbc1790bef5166468aa5453a
vitalii <vilavren@gmail.com>
2023-02-20 17:56:46 +0500

    added upload post

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import multer from 'multer'
+
 import {
   loginValidation,
   postCreateValidation,
@@ -18,17 +20,36 @@ mongoose
   .catch((err) => console.log('DB error', err))
 
 const app = express()
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads')
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname)
+  },
+})
+
+const upload = multer({ storage })
+
 app.use(express.json())
+app.use('/uploads', express.static('uploads'))
 
 app.post('/auth/login', loginValidation, UserController.login)
 app.post('/auth/register', registerValidation, UserController.register)
 app.get('/auth/me', checkAuth, UserController.getMe)
 
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  })
+})
+
 app.get('/posts', PostController.getAll)
 app.get('/posts/:id', PostController.getOne)
 app.post('/posts', checkAuth, postCreateValidation, PostController.create)
 app.delete('/posts/:id', checkAuth, PostController.remove)
-app.patch('/posts/:id', checkAuth, PostController.update)
+app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update)
 
 app.listen(4444, (err) => {
   if (err) {
