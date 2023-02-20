ac5087a0b9f4429972f5cd0fee194c49ccb105b5
vitalii <vilavren@gmail.com>
2023-02-21 00:30:25 +0500

    added function cors from lib

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import multer from 'multer'
+import cors from 'cors'
 
 import {
   loginValidation,
@@ -32,6 +33,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 app.use(express.json())
+app.use(cors())
 app.use('/uploads', express.static('uploads'))
 
 app.post(
