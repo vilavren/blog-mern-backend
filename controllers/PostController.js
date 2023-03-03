b9c6d7b06fcfed6587678d5d7ca3341950f47a99
vitalii <vilavren@gmail.com>
2023-03-03 05:43:01 +0500

    fix update tags post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -120,7 +120,7 @@ export const update = async (req, res) => {
         title: req.body.title,
         text: req.body.text,
         imageUrl: req.body.imageUrl,
-        tags: req.body.tags,
+        tags: req.body.tags.split(','),
         user: req.userId,
       }
     )
